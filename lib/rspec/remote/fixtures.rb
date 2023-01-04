c3b0b03b9bdad7370b97422063d8c4da479f4870
Aleks Clark <aleks.clark@gmail.com>
2023-01-04 14:04:28 -0600

    init

---
+++ lib/rspec/remote/fixtures.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "fixtures/version"
+
+module Rspec
+  module Remote
+    module Fixtures
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
